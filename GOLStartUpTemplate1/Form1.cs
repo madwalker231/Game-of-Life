83f7f3f118a8c452d18562340b7fa5c6e56ddb65
Madison Walker <madwalker231@gmail.com>
2022-10-26 13:35:53 -0700

    Added framework and code for NeighborCount Finite

--- GOLStartUpTemplate1/Form1.cs
+++ GOLStartUpTemplate1/Form1.cs
@@ -196,24 +196,69 @@ namespace GOLStartUpTemplate1
             graphicsPanel1.Invalidate();
         }
 
-        private void newToolStripButton_Click(object sender, EventArgs e)
+        private void PlayButtonMenu(object sender, EventArgs e)
         {
 
         }
 
-        private void toolStripButton1_Click(object sender, EventArgs e)
+        private void PauseButtonMenu(object sender, EventArgs e)
         {
 
         }
 
-        private void toolStripButton2_Click(object sender, EventArgs e)
+        private void NextGenButtonMenu(object sender, EventArgs e)
         {
 
         }
 
-        private void newToolStripMenuItem_Click(object sender, EventArgs e)
+        private void StopAtButtonMenu(object sender, EventArgs e)
         {
 
+        }
+
+        private int CountNeighborsFinite(int x, int y)
+        {
+            int count = 0;
+            int xLen = universe.GetLength(0);
+            int yLen = universe.GetLength(1);
+            for (int yOffset = -1; yOffset <= 1; yOffset++)
+            {
+                for (int xOffset = -1; xOffset <= 1; xOffset++)
+                {
+                    int xCheck = x + xOffset;
+                    int yCheck = y + yOffset;
+                    // if xOffset and yOffset are both equal to 0 then continue
+                    if (xOffset = 0 && yOffset = 0)
+                    {
+                        continue;
+                    }
+                    // if xCheck is less than 0 then continue
+                    if (xCheck < 0)
+                    {
+                        continue;
+                    }
+                    // if yCheck is less than 0 then continue
+                    if (yCheck < 0)
+                    {
+                        continue;
+                    }
+                    // if xCheck is greater than or equal too xLen then continue
+                    if (xCheck >= xLen)
+                    {
+                        continue;
+                    }
+                    // if yCheck is greater than or equal too yLen then continue
+                    if (yCheck >= yLen)
+                    {
+                        continue;
+                    }
+
+                    if (universe[xCheck, yCheck] == true) count++;
+                }
+            }
+            return xLen;
+            return yLen;
+            graphicsPanel1.Invalidate();
         }
     }
 }
